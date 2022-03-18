7c27451b5b16d67345079434ef737f7ca503b136
Ryan-Fec <97565763+Ryan-Fec@users.noreply.github.com>
2022-03-18 15:55:10 -0700

    Statistics

--- 3/main.cpp
+++ 3/main.cpp
@@ -2,15 +2,35 @@
 #include <random>
 #include <algorithm>
 #include <string>
+#include <vector>
+#include <numeric>
 #include "Utility.h"
 #include "Car.h"
 
 int main() {
     auto cars = randomCars();
     std::cout << "Cars: " << cars << "\n";
+    
+    std::vector<double> prices;
 
-    // TODO: print the average price and price range
-    std::cout << "The average price is: $" << "TODO" << "\n";
+    for (auto i = 0; i != cars.size(); i++) {
+        prices.push_back(cars.at(i).price());
+    }
+    double sum = std::accumulate(prices.begin(), prices.end(), 0.0);
 
-    std::cout << "The range is: $" << "TODO" << "\n";
+    double average = static_cast<double>(sum) / static_cast<double>(prices.size());
+
+    std::vector<double> range;
+
+    if (std::max_element(prices.cbegin(), prices.cend()) != prices.end()) {
+        range.push_back(*std::max_element(prices.cbegin(), prices.cend()));
+    }
+
+    if (std::min_element(prices.cbegin(), prices.cend()) != prices.end()) {
+        range.push_back(*std::min_element(prices.cbegin(), prices.cend()));
+    }
+
+    std::cout << "The average price is: $" << average << "\n";
+
+    std::cout << "The range is: $" << range << "\n";
 }
